70044f4c609ca8199b51ce5f5ea23854dc86a836
dad, dad (third party) <dad@timsoft-group.com>
2024-06-09 23:28:36 +0300

    ADD: show NotFound page on case of wrong URL.

--- src/appComponents/Router.js
+++ src/appComponents/Router.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import BooksGrid from "./productComponents/BooksGrid";
 import ProductDetails from "./productComponents/ProductDetails";
+import NotFound from "./pages/NotFound";
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<BooksGrid />} />
-            <Route path="/:productId" element={<ProductDetails />} />
+            <Route path="/details/:productId" element={<ProductDetails />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
---
+++ src/appComponents/pages/NotFound.js
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div className='page__not-found'>
+            Page not found!
+        </div>
+    )
+}
+
+export default NotFound;
